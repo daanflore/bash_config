fc6fa9ccca91e86ee5a88419ca96b03423a6e0a2
daanflore <daanflore@gmail.com>
2025-06-18 20:26:28 +0200

    Adding command duration

--- bashrc.sh
+++ bashrc.sh
@@ -55,6 +55,39 @@ if [[ "$-" != *i* ]]; then
     return
 fi
 
+
+# ======================
+# COMMAND DURATION TRACKING
+# ======================
+trap 'timer_start' DEBUG
+
+function timer_now {
+    date +%s%N
+}
+
+function timer_start {
+    timer_start=${timer_start:-$(timer_now)}
+}
+
+function timer_stop {
+    local delta_us=$((($(timer_now) - $timer_start) / 1000))
+    local us=$((delta_us % 1000))
+    local ms=$(((delta_us / 1000) % 1000))
+    local s=$(((delta_us / 1000000) % 60))
+    local m=$(((delta_us / 60000000) % 60))
+    local h=$((delta_us / 3600000000))
+    # Goal: always show around 3 digits of accuracy
+    if ((h > 0)); then timer_show=${h}h${m}m
+    elif ((m > 0)); then timer_show=${m}m${s}s
+    elif ((s >= 10)); then timer_show=${s}.$((ms / 100))s
+    elif ((s > 0)); then timer_show=${s}.$(printf %03d $ms)s
+    elif ((ms >= 100)); then timer_show=${ms}ms
+    elif ((ms > 0)); then timer_show=${ms}.$((us / 100))ms
+    else timer_show=${us}us
+    fi
+    unset timer_start
+}
+
 # ======================
 # GIT STATUS PROMPT
 # ======================
@@ -166,10 +199,14 @@ function _append_git_status() {
 # ======================
 function _buildPS1(){
     previousCommandResult="$?"
+
+    timer_stop
+
     _debug_log "Building PS1 prompt"
 
     local buildCommand=''
-    buildCommand+="${BPurple}[\$(date +%H:%M:%S)]${Color_Off}"
+
+    buildCommand+="\[${Yellow}\][${timer_show}]${Color_Off} "
 
     if [[ -v VIRTUAL_ENV ]]; then
         buildCommand+=" ${Yellow}(${VIRTUAL_ENV##*/})${Color_Off}"
